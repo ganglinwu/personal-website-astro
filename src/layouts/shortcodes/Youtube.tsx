2e21c5598e20c78d1252c5814fd1d0750650f7ea
ganglinwu <ganglin.wu@gmail.com>
2025-05-28 18:04:38 +0800

    removed react-lite-youtube-embed
    i couldnt get it to work
    
    it says there are no default exports but there clearly are
    
    i even checked all the files under
    node_modules/react-lite-youtube-embeds/dist

--- src/layouts/shortcodes/Youtube.tsx
+++ src/layouts/shortcodes/Youtube.tsx
@@ -1,24 +1,7 @@
 import React from "react";
-import LiteYouTubeEmbedComponent from "react-lite-youtube-embed";
-import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
-const Youtube = ({
-  id,
-  title,
-  ...rest
-}: {
-  id: string;
-  title: string;
-  [key: string]: any;
-}) => {
-  return (
-    <LiteYouTubeEmbedComponent
-      wrapperClass="yt-lite rounded-lg"
-      id={id}
-      title={title}
-      {...rest}
-    />
-  );
+const Youtube = () => {
+  return <div></div>;
 };
 
 export default Youtube;
